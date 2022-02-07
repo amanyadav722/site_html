10d61a157494b2ede0cfd1b301d517522c1bb34f
Adama995 <boyeadama6@gmail.com>
2022-02-07 17:44:50 +0100

    final commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FICHE AGENT</title>
+  </head>
+  <body style="background-color:#40E0D0	;">
+  <br/>
+    <div>
+        <div>
+            <span style="border: 3px solid black; padding: 10px;">IDENTIFICATION</span>
+        </div>
+        <div align="right">
+            <img src="img/img.jpeg" width="450" height="300"> 
+        </div>    
+    </div>
+
+    <br /><br />
+    <div id="liste_materiel" style="text-align:center">
+        <label>Mousqueton</label><input type="checkbox" name="chk"/><br />
+        <label>Gants d'intervention</label><input type="checkbox" name="chk"/><br />
+        <label>Ceinture de sécurité tactique</label><input type="checkbox" name="chk"/><br />
+        <label>Détecteur de métaux</label><input type="checkbox" name="chk"/><br />
+        <label>Brassard de sécurité</label><input type="checkbox" name="chk"/><br />
+        <label>Lampe torche</label><input type="checkbox" name="chk"/><br />
+        <label>Gilet par balle</label><input type="checkbox" name="chk"/><br />
+    </div>
+  </body>
+</html>
